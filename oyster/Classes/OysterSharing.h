27fcdcbae59c7d9c47c8b108ddce5840f3519377
Jesse Smith <jesses16@gmail.com>
2012-10-19 22:06:57 -0700

    Include NativeControls and further configure ShareKit

---
+++ oyster/Classes/OysterSharing.h
@@ -0,0 +1,13 @@
+//
+//  OsyterSharing.h
+//  Osyter
+//
+//  Created by Jesse Smith on 6/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DefaultSHKConfigurator.h"
+
+@interface OysterSharing : DefaultSHKConfigurator
+
+@end
